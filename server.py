393efea88532aa3a2cb369900a35fed6b9339cb8
LuizSim <a_simao@icloud.com>
2024-11-04 09:24:13 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from openpyxl.styles import PatternFill
 from pathlib import Path
 
 app = Flask(__name__)
-CORS(app)  # Permite requisições de todas as origens
+CORS(app) 
 
 responses = []
 
@@ -21,26 +21,20 @@ def save_response():
 @app.route('/', methods=['GET'])
 def generate_excel():
     if responses:
-        # Define o caminho do arquivo para salvar na pasta de downloads do usuário
         caminho_pasta_downloads = str(Path.home() / "Downloads")
 
-        # Salva o arquivo Excel dentro da pasta de downloads
         caminho_arquivo = os.path.join(caminho_pasta_downloads, 'resultado_ducks_math.xlsx')
         
         try:
-            # Criação da planilha usando openpyxl
             wb = Workbook()
             ws = wb.active
             ws.title = "Respostas"
 
-            # Adiciona o cabeçalho
             ws.append(["Perguntas", "Resposta", "Resposta do jogador", "Acertos", "Score"])
 
-            # Definir cores para acertos e erros
             green_fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
             red_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
 
-            # Preencher a planilha com as respostas
             for response in responses:
                 pergunta = response.get("question")
                 resposta_correta = response.get("correct_answer")
@@ -48,17 +42,14 @@ def generate_excel():
                 acerto = "Correto" if resposta_correta == resposta_jogador else "Incorreto"
                 score = response.get("score", 0)
 
-                # Adiciona a linha na planilha
                 ws.append([pergunta, resposta_correta, resposta_jogador, acerto, score])
 
-                # Aplicar cor na célula de "Acertos"
                 acertos_cell = ws.cell(row=ws.max_row, column=4)
                 if acerto == "Correto":
                     acertos_cell.fill = green_fill
                 else:
                     acertos_cell.fill = red_fill
 
-            # Salvar a planilha
             wb.save(caminho_arquivo)
 
             return jsonify({"message": "Arquivo Excel gerado com sucesso", "file_path": caminho_arquivo}), 200
